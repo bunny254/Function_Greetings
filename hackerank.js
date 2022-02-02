f73bf86932a095377bc3eceb94bca05da13a946a
unknown <kimaniwachira03@gmail.com>
2022-02-02 20:52:19 +0300

    Function and Function Calls using JavaScript

---
+++ hackerank.js
@@ -0,0 +1,8 @@
+function hackerank(parameterVariable="Welcome to 365 Days of JavaScript") {
+    console.log('Hello World!');
+    console.log(parameterVariable)
+       
+}
+hackerank();
+//console.log('Hello Kimani!');
+
